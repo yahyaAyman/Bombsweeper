cfc0691fc0e3f7493c71df883b6a5211288c4b85
Yahya Ayman <yahya_salamah@icloud.com>
2019-02-17 15:45:51 -0500

    Fixed Exit game issue

--- Start menu GUI.py
+++ Start menu GUI.py
@@ -2,6 +2,7 @@ import pygame
 from pygame import *
 from tkinter import Tk, Button
 import os
+import sys
 
 height_window = 700
 width_window = 700
@@ -25,22 +26,20 @@ def main():
     pygame.display.flip()
     play = True
     main_col = (0,0,0)
-    difficutly = (114,111,117)
+    difficutly_col = (114,111,117)
     main_options_font = pygame.font.SysFont("arialblack", 25)
     font = pygame.font.SysFont("arialblack", 18)
     game_name = main_options_font.render ("BombSweepr", 1 , main_col)
     options = main_options_font.render("Choose Game difficulty" ,1, main_col)
-    beginner = font.render("Beginner", 1, difficutly)
-    intermediate = font.render("Intermediate", 1,difficutly)
-    professional = font.render("Professional", 1, difficutly)
+    beginner = font.render("Beginner", 1, difficutly_col)
+    intermediate = font.render("Intermediate", 1,difficutly_col)
+    professional = font.render("Professional", 1, difficutly_col)
     quit_game = main_options_font.render("Quit", 1, main_col)
 
     ##Setting up text position on window
     window.blit(game_name, (100,50))
     window.blit(options, options_pos)
-##    window.blit(beginner,beginner_pos)
-##    window.blit(intermediate,intermediate_pos )
-##    window.blit(professional,  professional_pos)
+
     window.blit(quit_game, quit_pos)
 
     ##putting all the text on window together
@@ -49,15 +48,15 @@ def main():
     ##game loop
     while play:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 running = False
-                pygame.quit()
-                return exit()
+                pygame.display.quit()
+                sys.exit()
             
             if event.type ==  pygame.MOUSEBUTTONUP:
                 mouseX, mouseY = pygame.mouse.get_pos()
-                print(str(mouseX) + " and " + str(mouseY))
-                print(str(beginner_pos[0]) +" and "+ str(beginner_pos[1]))
+##                print(str(mouseX) + " and " + str(mouseY))
+##                print(str(beginner_pos[0]) +" and "+ str(beginner_pos[1]))
                 if (mouseX >= (options_pos[0] and 120) )and (mouseY <= (options_pos[1] and 400)):
                     window.blit(background,(0,0))
                     window.blit(options, options_pos)
@@ -65,11 +64,6 @@ def main():
                     window.blit(intermediate,intermediate_pos )
                     window.blit(professional,  professional_pos)
                     pygame.display.flip()
-
-
-
-
-
 
 if __name__ == '__main__':
     main()
