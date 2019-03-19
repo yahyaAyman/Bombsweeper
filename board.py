9e1a76769a3c76667eeb6f003254be1e18898d82
hanigm <47636869+hanigm@users.noreply.github.com>
2019-03-19 18:47:24 -0400

    click_tile fixed

--- board.py
+++ board.py
@@ -20,28 +20,19 @@ class Board:
                 self.grid[row][column].bomb = True;
                 
     def click_tile(self, row, column):
-        self.grid[row][column]                      #set clicked true for later recursive calls
+        self.grid[row][column].clicked = True #Set clicked true for later recursive calls
         if self.grid[row][column].bomb:             #if bomb was clicked
             return -1
         elif self.grid[row][column].bomb_num:       #if labeled tile was clicked
             return self.grid[row][column].bomb_num
-        else:                                       #if a blank tile was clicked
-            if row == 0:                            #if tile is on top row
-                R = (0 , 1)
-            elif row == self.row - 1:               #on bottom row
-                R = (-1 , 0)
-            else:                                   #middle rows
-                R = (-1, 0, 1)                      
-            if column == 0:                         #leftmost column
-                C = (0, 1)                          
-            elif column == self.column - 1:         #rightmost column
-                C = (-1 , 0)
-            else:                                   #middle columns
-                C = (-1, 0, 1)
-            for i in R:
-                for j in C:
-                    if not (self.grid[row +i][column + j].bomb or (self.grid[row + i][column + j].bomb_num > 0) or self.grid[row + i][column + j].clicked):
-                        click(self, row + i, row + j)
+        else :
+            R = self.row - 1
+            C = self.column -1
+            for i in (-1, 0, 1):
+                for j in (-1, 0, 1):
+                    if (0 <= i + row < R) and (0 <= j + column < C) and\
+                       (not self.grid[row + i][column + j].clicked or self.grid[row + i][column + j].bomb):
+                        self.click_tile(row + i, row + j)
         return 0
     
     def count_bomb_perimeter(self):
